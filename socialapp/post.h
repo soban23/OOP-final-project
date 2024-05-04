dfde7246a5145638ba347e9b4b17ef22d355882d
Soban <142923268+soban23@users.noreply.github.com>
2024-05-04 19:36:51 +0500

    Update post.h

--- socialapp/post.h
+++ socialapp/post.h
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+class post{
+private:
+  string id;
+  string title;
+  int likes;
+  string* likelist;
+  int comments;
+  string *commentlist;
+public:
+  post(){
+    id="";
+    title="";
+    likes=0;
+    likelist=nullptr;
+    comments=0;
+    commentlist=nullptr;
+    
+  }
+  void setrid(string i){
+    id=i;
+  }
+  void settitle(string t){
+    title=t;
+  }
+  void setlikes(int l){
+    likes=l;
+  }
+  void setcomments(int c){
+    comments=c;
+  }
+
+};
