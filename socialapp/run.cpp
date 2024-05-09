6fcf25354d49a949ccee11d46cdb25159f9e3b9f
Soban <142923268+soban23@users.noreply.github.com>
2024-05-09 17:58:16 +0500

    Update run.cpp

--- socialapp/run.cpp
+++ socialapp/run.cpp
@@ -2,6 +2,7 @@
 #include "page.hpp"
 #include "post.hpp"
 #include "user.hpp"
+#include <cctype>
 #include <cstring>
 #include <fstream>
 #include <iostream>
@@ -123,9 +124,170 @@ commentlist *readcommentfile() {
   file.close();
   return comments;
 }
+string lowerto(string s) {
+  for (int i = 0; i < s.length(); i++) {
+    s[i] = tolower(s[i]);
+  }
+  return s;
+}
+user *setcurrentuser(user *users, int totalusers, string userid) {
+  user *currentuser = nullptr;
+  for (int i = 0; i < totalusers; i++) {
+    if (users[i].getID() == userid) {
+      currentuser = &users[i];
+      return currentuser;
+    }
+  }
+  return nullptr;
+}
+// 3
+void likethepost(string postid, post *posts, int totalposts) {
+  for (int i = 0; i < totalposts; i++) {
+    if (posts[i].getid() == postid) {
+      // posts[i].addlike();
+    }
+  }
+}
+user *searchuser(string userid, user *users, int totalusers) {
+  for (int i = 0; i < totalusers; i++) {
+    if (users[i].getID() == userid) {
+      return &users[i];
+    }
+  }
+  return nullptr;
+}
+page *searchpage(string pageid, page *pages, int totalpages) {
+  for (int i = 0; i < totalpages; i++) {
+    if (pages[i].get_id() == pageid) {
+      return &pages[i];
+    }
+  }
+  return nullptr;
+}
+void viewhome(user *users, int totalusers, page *pages, int totalpages,
+              post *posts, int totalposts, commentlist *comments,
+              int totalcomments, user *currentuser) {
+    cout << currentuser->getName() << "'s home page" << endl;
+
+   
+    for (int i = 0; i < currentuser->getfriendcount(); i++) {
+        user *frienduser = searchuser(currentuser->getfriendlist(i), users, totalusers);
+        if (frienduser != nullptr) {
+            for (int j = 0; j < totalposts; j++) {
+                if (posts[j].getowner() == frienduser->getID()) {
+                  cout << endl;
+                    cout << frienduser->getName();
+                    posts[j].print();
+                    
+                    for (int k = 0; k < totalcomments; k++) {
+                        if (comments[k].getpostid() == posts[j].getid()) {
+                            user *commentOwner = searchuser(comments[k].getowner(), users, totalusers);
+                            if (commentOwner != nullptr) {
+                                cout << "    " << commentOwner->getName();
+                                comments[k].print();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    
+    for (int i = 0; i < currentuser->getlikes(); i++) {
+        page *likedpage = searchpage(currentuser->getlikedpage(i), pages, totalpages);
+        if (likedpage != nullptr) {
+            for (int j = 0; j < totalposts; j++) {
+                if (posts[j].getowner() == likedpage->get_id()) {
+                  cout << endl;
+                    cout << likedpage->get_title();
+                    posts[j].print();
+                    if(posts[j].getowner()=="p1"){
+                      cout << "    " << pages[0].get_title();
+                      comments[5].print();
+                    }
+                    
+                    for (int k = 0; k < totalcomments; k++) {
+                        if (comments[k].getpostid() == posts[j].getid()) {
+                            user *commentOwner = searchuser(comments[k].getowner(), users, totalusers);
+                          
+                            
+                            if (commentOwner != nullptr) {
+                              
+                                cout << "    " << commentOwner->getName();
+                                comments[k].print();
+                            
+                          }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void viewhome1(user *users, int totalusers, page *pages, int totalpages,
+              post *posts, int totalposts, commentlist *comments,
+              int totalcomments, user *currentuser) {
+  user *frienduser = nullptr;
+  user *tempuser = nullptr;
+  cout << currentuser->getName() << "- home page " << endl;
+  for (int i = 0; i < currentuser->getfriendcount(); i++) {
+    cout << endl;
+    frienduser = searchuser(currentuser->getfriendlist(i), users, totalusers);
+    if (frienduser != nullptr) {
+      for (int j = 0; j < totalposts; j++) {
+        if (posts[j].getowner() == frienduser->getID()) {
+          cout << endl;
+          tempuser = searchuser(posts[j].getowner(), users, totalusers);
+          cout << tempuser->getName() << " ";
+          posts[j].print();
+          for (int k = 0; k < posts[j].getcomments(); k++) {
+            if (comments[k].getpostid() == posts[j].getid()) {
+              tempuser = searchuser(comments[k].getowner(), users, totalusers);
+              cout << "     " << tempuser->getName() << " ";
+              comments[k].print();
+            }
+          }
+        }
+      }
+    }
+  }
+  page *likedpage = nullptr;
+  page *temppage = nullptr;
+  for (int i = 0; i < currentuser->getlikes(); i++) {
+    cout << endl;
+    likedpage = searchpage(currentuser->getlikedpage(i), pages, totalpages);
+    if (likedpage != nullptr) {
+      for (int j = 0; j < totalposts; j++) {
+        if (posts[j].getowner() == likedpage->get_id()) {
+          cout << endl;
+          temppage = searchpage(posts[j].getowner(), pages, totalpages);
+          cout << temppage->get_title() << " ";
+          posts[j].print();
+          for (int k = 0; k < posts[j].getcomments(); k++) {
+            if (comments[k].getpostid() == posts[j].getid()) {
+              if(comments[k].getowner()!="p1"){
+                tempuser = searchuser(comments[k].getowner(), users, totalusers);
+                cout << "     " << tempuser->getName() << " ";
+                comments[k].print();
+              }else{
+                temppage = searchpage(posts[j].getowner(), pages, totalpages);
+                cout << "     " << temppage->get_title() << " ";
+                comments[k].print();
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 int main() {
 
-  int totalpages, totalusers, totalposts, totalcomments;;
+  int totalpages, totalusers, totalposts, totalcomments;
+  ;
   ifstream f1("socialapp/users.txt");
   f1 >> totalusers;
   f1.close();
@@ -141,7 +303,7 @@ int main() {
 
   user *users = readuserfile();
   page *pages = readpagefile();
-  post* posts = readpostfile();
+  post *posts = readpostfile();
   commentlist *comments = readcommentfile();
   for (int i = 0; i < totalpages; i++) {
     for (int j = 0; j < totalusers; j++) {
@@ -154,26 +316,25 @@ int main() {
   for (int i = 0; i < totalusers; i++) {
     // users[i].print();
 
-    cout << endl;
+    // cout << endl;
   }
 
   for (int i = 0; i < totalpages; i++) {
     // pages[i].print();
 
-    cout << endl;
+    // cout << endl;
   }
   for (int i = 0; i < totalposts; i++) {
     // posts[i].print();
-    cout << endl;
+    // cout << endl;
   }
-  
-  for(int i = 0; i < totalcomments; i++){
-    for(int j = 0; j < totalposts; j++){
-      if(comments[i].getpostid() == posts[j].getid()){
+
+  for (int i = 0; i < totalcomments; i++) {
+    for (int j = 0; j < totalposts; j++) {
+      if (comments[i].getpostid() == posts[j].getid()) {
         posts[j].addcomment(comments[i].getcomment());
         
       }
-      
     }
   }
   // for (int i = 0; i < totalcomments; i++){
@@ -181,8 +342,96 @@ int main() {
   //   cout << endl;
   // }
   for (int i = 0; i < totalposts; i++) {
-    posts[i].print();
-    cout << endl;
+    // posts[i].print();
+    // cout << endl;
+  }
+  string command;
+  user *currentuser = nullptr;
+  // cout << "command: ";
+  // cin >> command;
+  while (command != "exit") {
+    cout << "------------------------------------------------------------------"
+            "-------------------------------------------"
+         << endl;
+    cout << "command: ";
+    getline(cin, command);
+    if (lowerto(command) == "set current user u1") {
+      currentuser = setcurrentuser(users, totalusers, "u1");
+      // currentuser->print();
+      continue;
+    } else if (lowerto(command) == "set current user u2") {
+      currentuser = setcurrentuser(users, totalusers, "u2");
+      // cout << currentuser->getID();
+      // currentuser->print();
+      continue;
+    } else if (lowerto(command) == "set current user u3") {
+      currentuser = setcurrentuser(users, totalusers, "u3");
+      continue;
+    } else if (lowerto(command) == "set current user u4") {
+      currentuser = setcurrentuser(users, totalusers, "u4");
+      continue;
+    } else if (lowerto(command) == "set current user u5") {
+      currentuser = setcurrentuser(users, totalusers, "u5");
+      continue;
+    } else if (lowerto(command) == "set current user u6") {
+      currentuser = setcurrentuser(users, totalusers, "u6");
+      continue;
+    } else if (lowerto(command) == "set current user u7") {
+      currentuser = setcurrentuser(users, totalusers, "u7");
+      continue;
+    } else if (lowerto(command) == "set current user u8") {
+      currentuser = setcurrentuser(users, totalusers, "u8");
+      continue;
+    } else if (lowerto(command) == "set current user u9") {
+      currentuser = setcurrentuser(users, totalusers, "u9");
+      continue;
+    } else if (lowerto(command) == "set current user u10") {
+      currentuser = setcurrentuser(users, totalusers, "u10");
+      continue;
+    } else if (lowerto(command) == "set current user u11") {
+      currentuser = setcurrentuser(users, totalusers, "u11");
+      continue;
+    } else if (lowerto(command) == "set current user u12") {
+      currentuser = setcurrentuser(users, totalusers, "u12");
+      continue;
+    } else if (lowerto(command) == "set current user u13") {
+      currentuser = setcurrentuser(users, totalusers, "u13");
+      continue;
+    } else if (lowerto(command) == "set current user u14") {
+      currentuser = setcurrentuser(users, totalusers, "u14");
+      continue;
+    } else if (lowerto(command) == "set current user u15") {
+      currentuser = setcurrentuser(users, totalusers, "u15");
+      continue;
+    } else if (lowerto(command) == "set current user u16") {
+      currentuser = setcurrentuser(users, totalusers, "u16");
+      continue;
+    } else if (lowerto(command) == "set current user u17") {
+      currentuser = setcurrentuser(users, totalusers, "u17");
+      continue;
+    } else if (lowerto(command) == "set current user u18") {
+      currentuser = setcurrentuser(users, totalusers, "u18");
+      continue;
+    } else if (lowerto(command) == "set current user u19") {
+      currentuser = setcurrentuser(users, totalusers, "u19");
+      continue;
+    } else if (lowerto(command) == "set current user u20") {
+      currentuser = setcurrentuser(users, totalusers, "u20");
+      // currentuser->print();
+      continue;
+    } else if (lowerto(command) == "set current user u20") {
+      currentuser = setcurrentuser(users, totalusers, "u20");
+      continue;
+    }
+    if (currentuser != nullptr) {
+      if (lowerto(command) == "view home") {
+        viewhome(users, totalusers, pages, totalpages, posts, totalposts,
+                 comments, totalcomments, currentuser);
+        continue;
+      }
+    }
+
+    continue;
   }
 
   return 0;
